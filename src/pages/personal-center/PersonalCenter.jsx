56a30a25a5d44a121764408fcbf1a42ee7d5bf06
王雅兰 <wangyalan1@bonc.com.cn>
2018-04-20 16:08:33 +0800

    个人中心-卡片展开和收起

--- src/pages/personal-center/PersonalCenter.jsx
+++ src/pages/personal-center/PersonalCenter.jsx
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react'
-import { Card, Row, Col } from 'antd'
+import { Card, Row, Col, Divider } from 'antd'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import ajaxUrl from 'config'
@@ -17,12 +17,27 @@ class PersonalCenter extends Component {
     super(props)
     this.state = {
       myApps: [],
-      myCollections: []
+      myCollections: [],
+      openStatus: {// 卡片展开：true,卡片收起：false
+        myApps: false,
+        studentApps: false,
+        myCollections: false
+      },
+      deleteActive: {// 删除按钮：false 确认/取消：true
+        myApps: false,
+        studentApps: false,
+        myCollections: false
+      },
+      deleteIds: {// 要删除的应用Id
+        myApps: [],
+        studentApps: [],
+        myCollections: []
+      }
     }
   }
 
   // 获取我的应用
-  getMyApps=() => {
+  getMyApps = () => {
     axios.post(ajaxUrl.personalApps, {}).then(res => {
       this.setState({
         myApps: res.data.data
@@ -31,12 +46,91 @@ class PersonalCenter extends Component {
   }
 
   // 获取我的收藏
-  getMyCollections=() => {
+  getMyCollections = () => {
     axios.post(ajaxUrl.personalCollections, {}).then(res => {
       this.setState({
         myCollections: res.data.data
       })
     }).catch(e => { console.log(e) })
+  }
+
+  // 确认删除
+  deleteConfirm = (type) => {
+    console.log('确认删除', type)
+    this.setState({
+      deleteActive: {
+        ...this.state.deleteActive,
+        [type]: false
+      }
+    })
+  }
+
+  // 取消删除
+  deleteCancel = (type) => {
+    console.log('取消删除', type)
+    this.setState({
+      deleteActive: {
+        ...this.state.deleteActive,
+        [type]: false
+      }
+    })
+  }
+
+  // 删除按钮
+  setDeleteActivity = (type) => {
+    console.log('删除点击', type)
+    this.setState({
+      deleteActive: {
+        ...this.state.deleteActive,
+        [type]: true
+      }
+    })
+  }
+
+  // 收起
+  takeUp = (type) => {
+    console.log('收起', type)
+    this.setState({
+      openStatus: {
+        ...this.state.openStatus,
+        [type]: false
+      }
+    })
+  }
+
+  // 展开
+  open = (type) => {
+    console.log('展开', type)
+    this.setState({
+      openStatus: {
+        ...this.state.openStatus,
+        [type]: true
+      }
+    })
+  }
+
+  // 卡片右上角删除和展开收起操作
+  getOpts = (type) => {
+    return (
+      <span className='opt-box' >
+        {
+          this.state.deleteActive[type]
+            ? (
+              <span>
+                <span className='link confirm' onClick={() => { this.deleteConfirm(type) }}>确认</span>
+                <span className='link cancel' onClick={() => { this.deleteCancel(type) }}>取消</span>
+              </span>
+            )
+            : <span className='link delete-btn' onClick={() => { this.setDeleteActivity(type) }}>删除</span>
+        }
+        <Divider type='vertical' />
+        {
+          this.state.openStatus[type]
+            ? <span className='link take-up' onClick={() => { this.takeUp(type) }} >收起</span>
+            : <span className='link open' onClick={() => { this.open(type) }} >展开</span>
+        }
+      </span>
+    )
   }
 
   componentDidMount () {
@@ -48,7 +142,7 @@ class PersonalCenter extends Component {
   render () {
     return (
       <div className='personal-center center-view mtb20'>
-        <Card title='我的应用' bordered={false} >
+        <Card title='我的应用' bordered={false} extra={this.getOpts('myApps')} className={this.state.openStatus['myApps'] ? 'open' : 'take-up'} >
           <Row gutter={16} >
             {
               this.state.myApps.map((item, index) => (
@@ -61,7 +155,7 @@ class PersonalCenter extends Component {
         </Card>
         {
           this.props.role === 'parents' && (
-            <Card title='学生应用' bordered={false} >
+            <Card title='学生应用' bordered={false} extra={this.getOpts('studentApps')} className={this.state.openStatus['studentApps'] ? 'open' : 'take-up'} >
               <Row gutter={16} >
                 {
                   this.state.myCollections.map((item, index) => (
@@ -74,7 +168,7 @@ class PersonalCenter extends Component {
             </Card>
           )
         }
-        <Card title='我的收藏' bordered={false} >
+        <Card title='我的收藏' bordered={false} extra={this.getOpts('myCollections')} className={this.state.openStatus['myCollections'] ? 'open' : 'take-up'} >
           <Row gutter={16} >
             {
               this.state.myCollections.map((item, index) => (
