60fa2d0cbe21145ac2ab9189edf45b10771844d4
王雅兰 <wangyalan1@bonc.com.cn>
2018-04-26 10:53:56 +0800

    统计分析页面 接口对接后端 增加应用选择下拉框 实现搜索功能

--- src/pages/statistical-analysis/StatisticalAnalysis.jsx
+++ src/pages/statistical-analysis/StatisticalAnalysis.jsx
@@ -4,43 +4,59 @@
 
 import React, { Component } from 'react'
 import './StatisticalAnalysis.scss'
-import { Row, Col, Card, DatePicker, Button } from 'antd'
+import { Row, Col, Card, DatePicker, Button, Select } from 'antd'
 import axios from 'axios'
 import _ from 'lodash'
 import ajaxUrl from 'config'
 import Echarts from '../../components/common/Echarts'
 import getEchartsOptions from '../../utils/getEchartsOptions'
 
+const Option = Select.Option
+
 class StatisticalAnalysis extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      downloadLineData: {},
-      appTypeRadioData: [],
-      downloadTypeRadioData: []
+      downloadLineData: {}, // 软件下载量变化
+      appTypeRadioData: [], // 应用类型占比
+      downloadTypeRadioData: [], // 当月应用下载型占比
+      allAppCode: []// 所有应用码表
     }
+    // 记录两个搜索条件，日期范围和选择的应用id
     this.dateRange = {
       startDate: '',
       endDate: ''
     }
+    this.appCode = ''
   }
 
+  // 日期变动
   dateChange = (date, dateString) => {
     console.log(date, dateString)
     this.dateRange = {
-      starDate: dateString[0],
+      startDate: dateString[0],
       endDate: dateString[1]
     }
   }
 
   // 搜索
   search = () => {
-    console.log('搜索', this.dateRange)
+    console.log('搜索', this.dateRange, this.appCode)
+    this.getDownloadLineData({
+      time1: this.dateRange.startDate,
+      time2: this.dateRange.endDate,
+      sw_id: this.appCode
+    })
   }
 
   // 软件下载量变化
-  getDownloadLineData=() => {
-    axios.post(ajaxUrl.softwareDownload, {}).then(res => {
+  getDownloadLineData=(params) => {
+    axios.post(ajaxUrl.softwareDownload, {
+      time1: '',
+      time2: '',
+      sw_id: '',
+      ...params
+    }).then(res => {
       console.log(res.data)
       this.setState({
         downloadLineData: res.data
@@ -68,6 +84,22 @@ class StatisticalAnalysis extends Component {
     }).catch(e => { console.log(e) })
   }
 
+  // 获取统计分析全部软件下拉列表
+  getAllApps=() => {
+    axios.post(ajaxUrl.getAllAppCode, {}).then(res => {
+      console.log(res.data)
+      this.setState({
+        allAppCode: res.data.data
+      })
+    }).catch(e => { console.log(e) })
+  }
+
+  // 应用选择
+  appChange = (value) => {
+    console.log(`selected ${value}`)
+    this.appCode = value
+  }
+
   componentDidMount () {
     // 软件下载量变化
     this.getDownloadLineData()
@@ -75,14 +107,23 @@ class StatisticalAnalysis extends Component {
     this.getAppTypeRadioData()
     // 当月应用下载型占比
     this.getDownloadTypeRadioData()
+    // 获取统计分析全部软件下拉列表
+    this.getAllApps()
   }
 
   render () {
-    const { downloadLineData, appTypeRadioData, downloadTypeRadioData } = this.state
+    const { downloadLineData, appTypeRadioData, downloadTypeRadioData, allAppCode } = this.state
     return (
       <div className='statistical-analysis center-view mtb20'>
         <Card title='统计分析' bordered={false}>
           <div>
+            <Select onChange={this.appChange} placeholder='请选择应用' >
+              {
+                allAppCode.map((item, index) => (
+                  <Option key={item.sw_id} >{item.sw_name}</Option>
+                ))
+              }
+            </Select>
             <DatePicker.RangePicker onChange={this.dateChange} />
             <Button type='primary' onClick={this.search} >搜索</Button>
           </div>
