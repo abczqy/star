0bb5eb3844f1d0faee4111f724a730264b8bf890
chaihaorui <chaihaorui@bonc.com.cn>
2018-04-26 14:13:30 +0800

    待审核详情接口联调

--- static/Config.js
+++ static/Config.js
@@ -37,6 +37,9 @@ window.mungConfig = {
   // 运营管理/软件管理-待审核列表
   waitVerify: AJAX_HOST + '/management/examinelistm',
 
+  // 运营管理/软件管理-迭代审核、待审核详情
+  verityDetail: AJAX_HOST + '/management/appdetail',
+
   // 运营管理/会员管理-厂商
   getFactory: AJAX_HOST + '/factoryManage/getFactory',
 
