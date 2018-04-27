b9533d0781092deddda946a4f14ab410f588a812
王雅兰 <wangyalan1@bonc.com.cn>
2018-04-27 18:10:10 +0800

    统计分析-环形图样式

--- src/utils/getEchartsOptions.js
+++ src/utils/getEchartsOptions.js
@@ -11,8 +11,10 @@ const getEchartsOptions = (data, type, title, config) => {
   switch (type) {
     case 'pie-doughnut':
       let legendDatas = []
+      let sum = 0
       data.forEach((item, index) => {
-        legendDatas.push(item.name)
+        legendDatas.push(item.name + 'ddd')
+        sum += item.value
       })
       options = {
         title: {
@@ -25,49 +27,65 @@ const getEchartsOptions = (data, type, title, config) => {
         },
         tooltip: {
           trigger: 'item',
-          // formatter: '{a} <br/>{b}: {c} ({d}%)'
-          formatter: '{b}: {c} ({d}%)'
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+          // formatter: '{b}: {c} ({d}%)'
         },
         legend: {
           orient: 'vertical',
           x: 'right',
           y: 'middle',
-          data: legendDatas
+          'icon': 'circle'
         },
         color: ['#f0647e', '#58acfb', '#fad352'], // 配置颜色
-        series: [
-          {
-            name: '',
-            type: 'pie',
-            radius: ['50%', '70%'],
-            avoidLabelOverlap: false,
-            label: {
-              normal: {
-                show: false,
-                position: 'center'
-              },
-              emphasis: {
-                show: true,
-                textStyle: {
-                  fontSize: '20',
-                  fontWeight: 'bold'
+        series: [{
+          name: title,
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          hoverOffset: 0,
+          hoverAnimation: false,
+          label: {
+            normal: {
+              show: true,
+              position: 'center',
+              color: '#666',
+              formatter: [
+                '{title|应用总数}',
+                `{num|${sum}}`
+              ].join('\n'),
+              rich: {
+                title: {
+                  color: '#999',
+                  fontSize: 12
+                },
+                num: {
+                  color: '#333',
+                  fontSize: 20,
+                  lineHeight: 36
                 }
               }
             },
-            itemStyle: {
-              normal: {// 设置环形图断开 其实是背景色的border
-                borderWidth: 6,
-                borderColor: '#ffffff'
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-            data
-          }
-        ]
+            emphasis: {
+              show: false
+              // textStyle: {
+              //   fontSize: '20',
+              //   fontWeight: 'bold'
+              // }
+            }
+          },
+          itemStyle: {
+            normal: { // 设置环形图断开 其实是背景色的border
+              borderWidth: 6,
+              borderColor: '#ffffff'
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data
+        }]
       }
       break
     case 'area-stack':
@@ -75,18 +93,16 @@ const getEchartsOptions = (data, type, title, config) => {
       let seriesData = []
       data.data.forEach((item, index) => {
         legendDatasLine.push(item.name)
-        seriesData.push(
-          {
-            name: item.name,
-            type: 'line',
-            stack: '总量',
-            areaStyle: {
-              color: 'rgba(45,183,245,0.5)'
-            },
-            smooth: true,
-            data: item.value
-          }
-        )
+        seriesData.push({
+          name: item.name,
+          type: 'line',
+          stack: '总量',
+          areaStyle: {
+            color: 'rgba(45,183,245,0.5)'
+          },
+          smooth: true,
+          data: item.value
+        })
       })
       options = {
         title: {
@@ -117,18 +133,14 @@ const getEchartsOptions = (data, type, title, config) => {
           bottom: '3%',
           containLabel: true
         },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: false,
-            data: data.xAxis
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
+        xAxis: [{
+          type: 'category',
+          boundaryGap: false,
+          data: data.xAxis
+        }],
+        yAxis: [{
+          type: 'value'
+        }],
         color: ['#2db7f5', '#808bc6'],
         series: seriesData
 
