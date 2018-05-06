b417b872b38104371c3ac8030325078ecb9c2649
乔阳 <qiaoyang@bonc.com.cn>
2018-05-06 20:10:04 +0800

    后台管理-平台管理-新闻列表-编辑

--- src/pages/software-market/platform-manage/news-list/news-list-edit/NewsListEdit.jsx
+++ src/pages/software-market/platform-manage/news-list/news-list-edit/NewsListEdit.jsx
@@ -3,8 +3,10 @@ import { Card, Icon, Button, Col, Row, Input, Upload } from 'antd'
 import { Link } from 'react-router-dom'
 import PropsTypes from 'prop-types'
 import Editor from 'wangeditor'
+import ajaxUrl from 'config'
 import {
-  getNewsListForEdit
+  getNewsListForEdit,
+  updateNewsList
 } from 'services/software-manage'
 import { BlankBar } from 'components/software-market'
 import './NewsListEdit.scss'
@@ -13,7 +15,11 @@ class NewsListEdit extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      data: {}
+      data: {},
+      fileList: [],
+      postParam: {
+        newsTilte: null
+      }
     }
   }
 
@@ -32,6 +38,44 @@ class NewsListEdit extends Component {
     return this.editor.txt.text()
   }
 
+  /**
+   * 获取标题的并写入state中
+   */
+  getTitle = (e) => {
+    this.setState({
+      postParam: {
+        ...this.state.postParam,
+        newsTilte: e.target.value
+      }
+    })
+  }
+
+  /**
+   * 返回附件的参数
+   * @returns {*}
+   */
+  getFormData (pass) {
+    const { fileList, data, postParam } = this.state
+    const formData = new FormData()
+    formData.append('news_id', data.news_id)
+    formData.append('news_title', postParam.news_title)
+    formData.append('news_desc', this.getRichText())
+    fileList.forEach((file) => {
+      formData.append('news_picture', file)
+    })
+    return formData
+  }
+
+  /**
+   * 添加
+   */
+  subMit = () => {
+    const param = this.getFormData
+    updateNewsList(param, (res) => {
+      console.log(`${res.data.info}`)
+    })
+  }
+
   componentDidMount () {
     // 创建富文本编辑器
     const mountedElem = this.refs.mountedElem
@@ -42,7 +86,13 @@ class NewsListEdit extends Component {
     getNewsListForEdit({news_id: searchId}, (res) => {
       const data = res.data
       this.setState({
-        data: data
+        data: data,
+        fileList: [{
+          uid: -1,
+          name: 'xxx.png',
+          status: 'done',
+          url: ajaxUrl.IMG_BASE_URL + '/' + data.news_picture
+        }]
       }, () => {
         console.log(`data: ${data.news_desc}`)
         // 设置富文本编辑器中的初始内容
@@ -52,7 +102,34 @@ class NewsListEdit extends Component {
   }
 
   render () {
-    const { data } = this.state
+    const uploadButton = (
+      <div>
+        <Icon type='plus' />
+        <div className='ant-upload-text'>Upload</div>
+      </div>
+    )
+    const props = {
+      onRemove: (file) => {
+        console.log('移除附件')
+        this.setState(({ fileList }) => {
+          const index = fileList.indexOf(file)
+          const newFileList = fileList.slice()
+          newFileList.splice(index, 1)
+          return {
+            fileList: newFileList
+          }
+        })
+      },
+      beforeUpload: (file) => {
+        console.log('上传之前')
+        this.setState(({ fileList }) => ({
+          fileList: [...fileList, file]
+        }))
+        return false
+      },
+      fileList: this.state.fileList
+    }
+    const { data, fileList } = this.state
     return (
       <div className='news-list-wrap' >
         <Card title='编辑新闻' extra={<Link to='/software-market-home/platform-manage/news-list'><Icon type='double-left' />返回列表页</Link>}>
@@ -61,16 +138,24 @@ class NewsListEdit extends Component {
               <Row gutter={16}>
                 <Col span={12}>
                   <span className='edit-bar-left-label'>通知标题: </span>
-                  <Input className='edit-bar-right-Input' placeholder={'' + data.news_title} />
+                  <Input
+                    className='edit-bar-right-Input'
+                    placeholder={'' + data.news_title}
+                    onChange={this.getTitle}
+                  />
                 </Col>
                 <Col span={6}>
                   <span className='pic-card-label'>上传图片: </span>
-                  <Upload listType='picture-card' {...this.uploadParams} className='pic-card' >
-                    <Icon type='upload' />
+                  <Upload
+                    listType='picture-card'
+                    {...props}
+                    fileList={fileList}
+                  >
+                    {fileList.length >= 1 ? null : uploadButton}
                   </Upload>
                 </Col>
                 <Col span={6}>
-                  <Upload {...this.uploadParams} >
+                  <Upload disabled >
                     <Button className='upload-btn'><Icon type='upload' />上传文件</Button>
                   </Upload>
                   <span className='marks-font-type'>支持扩展名: .jpg .png (100px * 180px)</span>
@@ -90,7 +175,7 @@ class NewsListEdit extends Component {
             </Link>
             <span className='blank-bar-ver' />
             <Link to='/software-market-home/platform-manage/news-list'>
-              <Button type='primary'>添加</Button>
+              <Button type='primary' onClick={this.subMit}>添加</Button>
             </Link>
           </span>
         </div>
