c4918769b2d6b75c70c6883b60d1f58a255de67e
乔阳 <qiaoyang@bonc.com.cn>
2018-05-07 09:33:21 +0800

    会员管理-下拉框-接口调试

--- src/utils/utils-sw-manage/dataForSelect.jsx
+++ src/utils/utils-sw-manage/dataForSelect.jsx
@@ -7,23 +7,27 @@
    * @param {obj} thiz 把调用改函数的this环境传进来 以对调用者的环境state进行操作
    */
 const getSelectList = (sunFn, typeVal, key, thiz) => {
-  let list = {}
   // list['idList'] = ['123', '234', '345']
   const reqParam = typeVal ? {type: typeVal} : null
+  console.log(`reqParam: ${JSON.stringify(reqParam)}`)
   // 向后台请求数据
   sunFn(reqParam, (res) => {
     const data = res.data
-    list[key] = data.data
+    let list = {}
+    list[key] = data.list
+    // 设置selectList数据到调用者state中
+    thiz.setState({
+      selectList: {
+        ...thiz.state.selectList,
+        ...list
+      }
+    })
+
+    console.log(`工具函数中-请求回调内-原始数据：${data.list}`)
+    console.log(`工具函数中-请求回调内-添加数据：${list[key]}`)
+    console.log(`工具函数中：${JSON.stringify(list)}`)
   })
   // 加个key ??
-
-  // 设置selectList数据到调用者state中
-  thiz.setState({
-    selectList: {
-      ...thiz.state.selectList,
-      ...list
-    }
-  })
 }
 
 export { getSelectList }
