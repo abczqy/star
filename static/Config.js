726050f1bd403a17c58182b1c7ba7c7e58827b1f
chaihaorui <chaihaorui@bonc.com.cn>
2018-05-16 11:12:37 +0800

    移除static/config中的没用到的接口

--- static/Config.js
+++ static/Config.js
@@ -11,36 +11,6 @@ window.mungConfig = {
   APP_COUNT: 'http://localhost:8080/static/operation-statistical/statistics/appStatistics.html', // 后台管理系统->运营统计->应用统计
   /** *******************************软件市场************************************ */
 
-  // 分享列表接口
-  detList: AJAX_HOST + '/application/detList', // 现在公告和详情页分享列表用的是这个
-
-  /** *******************************注册************************************ */
-  registerValitemail: AJAX_HOST + '/register/vailEmail', // 校验邮箱是否被占用
-  register: AJAX_HOST + '/register/registermaf', // 注册按钮
-  /* 账号设置 */
-  // 家长注册接口-查询与家长绑定的学生
-  relationQueryStu: AJAX_HOST + '/addRelation/queryStu',
-  // 家长注册接口-与学生绑定
-  relationAdd: AJAX_HOST + '/addRelation/add',
-  // 家长注册接口-查询与家长绑定的学生
-  relationdelete: AJAX_HOST + '/addRelation/delete',
-  // 登出
-  sessionLogout: AJAX_HOST + '/session/logout',
-  // 消息通知列表
-  getAllMessageList: AJAX_HOST + '/getAllMessageList',
-  // 未读消息数
-  getMessageCount: AJAX_HOST + '/getUnreadMessageCount',
-  // 消息通知列表详情
-  getMessageListDetail: AJAX_HOST + '/getMessageDetail',
-  // 账号设置-密码修改
-  updateUserPassword: AJAX_HOST + '/control/updateUserPassword',
-  // 账号设置-手机验证
-  updatePhoneNum: AJAX_HOST + '/updatePhoneNum',
-  // 忘记密码
-  forgetThePassword: AJAX_HOST + '/control/updateUserPassword',
-  // 获取短信验证码
-  SMSVerification: AJAX_HOST + '/SMSVerification',
-
   // 门户首页-门户导航
   addGatewayNavigation: AJAX_HOST + '/addGatewayNavigation',
   // 门户导航-查询接口
