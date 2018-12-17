1bcf943bcb4403d1cde3b5ad22bb4d6b4a6f5e5d
qiaoyang <qiaoyang@bonc.com.cn>
2018-12-17 09:24:29 +0800

    配置-baseUrl的修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 
 const Config = {
-  API_BASE_URL: 'http://p1663488m8.imwork.net:49784/server',
-  IMG_BASE_URL: 'http://p1663488m8.imwork.net:15206',
+  API_BASE_URL: 'http://fjedu.nebedu.cn/server',
+  IMG_BASE_URL: 'http://fjdownload.nebedu.cn',
   BASE_TAB: 'http://localhost:8080',
   USER_COUNT: 'http://localhost:8080/static/operation-statistical/statistics/userStatistics.html', // 后台管理系统->运营统计->用户统计
   APP_COUNT: 'http://localhost:8080/static/operation-statistical/statistics/appStatistics.html', // 后台管理系统->运营统计->应用统计
