f874e2297ee82f34d10c3972be92ee7b30ac66d8
qiaoyang <qiaoyang@bonc.com.cn>
2018-12-19 10:57:23 +0800

    后台管理-路由菜单建立

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as Page } from './page'
 export { default as Align } from './align'
 export { default as UploadFile } from './uploadFile'
+export { default as MenuItemsCreater } from './menu-items-creater'
--- src/views/SoftwareMarket.jsx
+++ src/views/SoftwareMarket.jsx
@@ -4,23 +4,18 @@
  * 软件市场入口
  */
 import React from 'react'
-import { Layout, Menu, Icon } from 'antd'
-// import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { Layout, Icon } from 'antd'
 import { renderRoutes } from 'react-router-config'
 import {
   Businessing
 } from 'pages/software-market'
-import ImgSoft from '../assets/images/operationMana/u103.png'
-import ImgMemb from '../assets/images/operationMana/u15.png'
-import ImgPlat from '../assets/images/operationMana/u57.png'
-import ImgOper from '../assets/images/operationMana/u83.png'
 import SignOut from './SignOut'
 import { withRouter, Route } from 'react-router'
+import { MenuItemsCreater } from '../components/common'
+import menuData from './data/menuData'
 
-const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
-// import { withRouter } from 'react-router-dom'
+
 class SoftwareMarket extends React.Component {
   constructor (props) {
     super(props)
@@ -91,73 +86,15 @@ class SoftwareMarket extends React.Component {
             </Header>
             <Layout className='sider-bar'>
               <Sider width={200}>
-                <Menu
+                <MenuItemsCreater
+                  data={menuData}
                   mode='inline'
                   theme='dark'
                   defaultSelectedKeys={['1']}
                   defaultOpenKeys={['sub1']}
                   style={{ height: '100%', borderRight: 0 }}
                   onClick={({ item, key, keyPath }) => this.renderContent({ item, key, keyPath })}
-                >
-                  <SubMenu
-                    key='sub1'
-                    title={<span><img style={{ width: 14, height: 14, marginRight: 10, marginTop: -3 }} src={ImgSoft} />软件管理</span>} >
-                    <Menu.Item key='1'>
-                      <Link to='/software-market-home/software-manage/businessing'>运营中</Link>
-                    </Menu.Item>
-                    <Menu.Item key='2'>
-                      <Link to='/software-market-home/software-manage/wait-verify'>待审核</Link>
-                    </Menu.Item>
-                    <Menu.Item key='3'>
-                      <Link to='/software-market-home/software-manage/iteration-verify'>迭代审核</Link>
-                    </Menu.Item>
-                  </SubMenu>
-                  <SubMenu key='sub2' title={<span><img style={{ width: 14, height: 14, marginRight: 10, marginTop: -3 }} src={ImgMemb} />会员管理</span>}>
-                    <Menu.Item key='5'>
-                      <Link to='/software-market-home/member-manage/manufacturer'>厂商</Link>
-                    </Menu.Item>
-                    <Menu.Item key='6'>
-                      <Link to='/software-market-home/member-manage/school'>学校</Link>
-                    </Menu.Item>
-                    <Menu.Item key='7'>
-                      <Link to='/software-market-home/member-manage/teacher'>老师</Link>
-                    </Menu.Item>
-                    <Menu.Item key='8'>
-                      <Link to='/software-market-home/member-manage/student'>学生</Link>
-                    </Menu.Item>
-                    <Menu.Item key='81'>
-                      <Link to='/software-market-home/member-manage/parent'>家长</Link>
-                    </Menu.Item>
-                    <Menu.Item key='82'>
-                      <Link to='/software-market-home/member-manage/educational-services'>教育机构</Link>
-                    </Menu.Item>
-                  </SubMenu>
-                  <SubMenu key='sub3' title={<span><img style={{ width: 14, height: 14, marginRight: 10, marginTop: -3 }} src={ImgPlat} />平台管理</span>}>
-                    <Menu.Item key='9'>
-                      <Link to='/software-market-home/platform-manage/porttal-homepage'>门户首页</Link>
-                    </Menu.Item>
-                    <Menu.Item key='10'>
-                      <Link to='/software-market-home/platform-manage/news-list'>新闻列表</Link>
-                    </Menu.Item>
-                    <Menu.Item key='11'>
-                      <Link to='/software-market-home/platform-manage/public-info'>信息公开</Link>
-                    </Menu.Item>
-                    <Menu.Item key='12'>
-                      <Link to='/software-market-home/platform-manage/public-info-verify'>信息公开审核</Link>
-                    </Menu.Item>
-                    <Menu.Item key='13'>
-                      <Link to='/software-market-home/platform-manage/plat-homepage'>平台首页</Link>
-                    </Menu.Item>
-                  </SubMenu>
-                  <SubMenu key='sub4' title={<span><img style={{ width: 14, height: 14, marginRight: 10, marginTop: -3 }} src={ImgOper} />运营统计</span>}>
-                    <Menu.Item key='sub4-apply'>
-                      <Link to='/software-market-home/platform-manage/app-count'>应用统计</Link>
-                    </Menu.Item>
-                    <Menu.Item key='sub4-satis'>
-                      <Link to='/software-market-home/platform-manage/user-count'>用户统计</Link>
-                    </Menu.Item>
-                  </SubMenu>
-                </Menu>
+                />
               </Sider>
               <Layout style={{ padding: '20px ' }}>
                 {
