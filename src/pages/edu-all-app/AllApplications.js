0670a5175c83f44571ef013e7c422e14c27f5437
zhangyanru <zhangyanru@bonc.com.cn>
2018-12-29 15:47:07 +0800

    软件超市全部应用联调

--- src/pages/edu-all-app/AllApplications.js
+++ src/pages/edu-all-app/AllApplications.js
@@ -8,12 +8,12 @@ import Config from 'config'
 import { Layout, Menu, Row, Col, Input } from 'antd'
 import PropTypes from 'prop-types'
 // import { renderRoutes } from 'react-router-config'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import allApp from '../../assets/images/all-app/u2835.jpg'
 import {Logged} from 'components/common/hoc/Logged'
 import { Route } from 'react-router'
 import AllApplicationsDetail from 'pages/edu-all-app/AllApplicationsDetail'
-import {getAppType, allAppList, allAppPlatformList} from 'services/all-app/'
+import {getAppType, allAppList} from 'services/all-app/'
 import {getPortalBannerImg} from 'services/portalnew/'
 import './AllApplications.css'
 const Search = Input.Search
@@ -29,6 +29,9 @@ export default class AllApplications extends React.Component {
       platformAppDataa: [],
       mode: 'inline',
       theme: 'light',
+      shelfTimeSort: 'desc',
+      downloadNum: 'decsc',
+      key: '0',
       menuData: [] // 存全部应用的分类
     }
   }
@@ -37,6 +40,129 @@ export default class AllApplications extends React.Component {
     allAppListData: PropTypes.array,
     platformAppDataa: PropTypes.array
   }
+  // 上架时间处理
+  handleShelfTime = () => {
+    console.log('上架时间')
+    if (this.state.shelfTimeSort === 'desc') {
+      this.setState({
+        shelfTimeSort: 'asc'
+      }, () => {
+        // 获取软件应用数据
+        this.getAppListRj({
+          sort: 'asc',
+          orderType: 'time',
+          appType: this.state.key
+        })
+        // // 获取平台应用
+        // this.getAppListPt({
+        //   sort: 'asc',
+        //   orderType: 'time',
+        //   appType: this.state.key,
+        //   platformType: 'pt'
+        // })
+      })
+    } else {
+      this.setState({
+        shelfTimeSort: 'desc'
+      }, () => {
+        // 获取软件应用数据
+        this.getAppListRj({
+          sort: 'desc',
+          orderType: 'time',
+          appType: this.state.key
+        })
+        // // 获取平台应用
+        // this.getAppListPt({
+        //   sort: 'desc',
+        //   orderType: 'time',
+        //   appType: this.state.key,
+        //   platformType: 'pt'
+        // })
+      })
+    }
+  }
+  // 下载量处理
+  handleDownloadNum = () => {
+    console.log('下载量')
+    if (this.state.downloadNum === 'desc') {
+      this.setState({
+        downloadNum: 'asc'
+      }, () => {
+        // 获取软件应用数据
+        this.getAppListRj({
+          sort: 'asc',
+          orderType: 'download',
+          appType: this.state.key
+        })
+        // // 获取平台应用
+        // this.getAppListPt({
+        //   sort: 'asc',
+        //   orderType: 'download',
+        //   appType: this.state.key,
+        //   platformType: 'pt'
+        // })
+      })
+    } else {
+      this.setState({
+        downloadNum: 'desc'
+      }, () => {
+        // 获取软件应用数据
+        this.getAppListRj({
+          sort: 'desc',
+          orderType: 'download',
+          appType: this.state.key
+        })
+        // // 获取平台应用
+        // this.getAppListPt({
+        //   sort: 'desc',
+        //   orderType: 'download',
+        //   appType: this.state.key,
+        //   platformType: 'pt'
+        // })
+      })
+    }
+  }
+  // 获取软件应用数据
+  getAppListRj = (params) => {
+    allAppList(params, (res) => {
+      console.log('获取软件应用数据', res.data.data.data)
+      this.setState({
+        allAppListData: res.data.data.data || []
+      })
+    }).catch((e) => { console.log(e) })
+  }
+  // 获取平台应用数据
+  getAppListPt = (params) => {
+    allAppList(params, (res) => {
+      console.log('获取平台应用数据', res.data.data.data)
+      var result = []
+      for (var i = 0; i < res.data.data.data.length; i += 6) {
+        result.push(res.data.data.data.slice(i, i + 6))
+      }
+      console.log('获取平台应用数据---转化', result)
+      this.setState({
+        platformAppDataa: result || []
+      })
+    }).catch((e) => { console.log(e) })
+  }
+  // 获取分类对应的平台应用和软件应用
+  getTypeData = ({ item, key, keyPath }) => {
+    console.log('选中', key)
+    this.setState({
+      key: key
+    }, () => {
+      // 获取软件应用数据
+      this.getAppListRj({
+        appType: key
+      })
+      // 获取平台应用
+      this.getAppListPt({
+        appType: key,
+        platformType: 'pt'
+      })
+    })
+  }
+
   componentDidMount () {
     // 获取全部应用页面的轮播图
     getPortalBannerImg({
@@ -54,41 +180,24 @@ export default class AllApplications extends React.Component {
       }
     })
 
+    // 获取全部软件应用数据 appType platformType 默认是软件应用
+    this.getAppListRj()
+    // 获取全部平台应用
+    this.getAppListPt({
+      platformType: 'pt'
+    })
+
     // 获取应用分类
     getAppType({}, (response) => {
       if (response.data.code === 200) {
         let result = response.data.data
-        // console.log('获取应用分类', result)
+        console.log('获取应用分类', result)
         this.setState({
           menuData: result || []
         })
       } else {
         console.log('getAppType出现异常:', response.data.msg || '')
       }
-    }).catch((e) => { console.log(e) })
-
-    // 获取软件应用数据
-    allAppList({
-      appType: '0'
-    }, (res) => {
-      // console.log('获取软件应用数据', res.data.data.data)
-      this.setState({
-        allAppListData: res.data.data.data || []
-      })
-    }).catch((e) => { console.log(e) })
-
-    // 获取平台应用数据
-    allAppPlatformList({
-    }, (res) => {
-      // console.log('获取平台应用数据', res.data)
-      this.setState({
-        platformAppData: res.data.data || []
-      }, () => {
-        this.state.platformAppDataa = []
-        for (let i in this.state.platformAppData) {
-          this.state.platformAppDataa.push(this.state.platformAppData[i])
-        }
-      })
     }).catch((e) => { console.log(e) })
   }
 
@@ -136,14 +245,16 @@ export default class AllApplications extends React.Component {
           <Sider >
             <Menu
               style={{ width: 256, height: 800, textAlign: 'center', border: 0, boxShadow: '2px 2px 5px #999' }}
-              defaultSelectedKeys={['1']}
+              defaultSelectedKeys={['0']}
               defaultOpenKeys={['sub1']}
               mode={this.state.mode}
               theme={this.state.theme}
+              onClick={this.getTypeData}
             >
               {this.state.menuData && this.state.menuData.map((item, index) => {
-                return <Menu.Item key={index} style={{height: 60, lineHeight: '60px', fontSize: 16}}>
-                  <Link to={'/operate-manage-home/all-app/all-app?' + item.APP_TYPE_ID || ''}>{item.APP_TYPE_NAME || '无'}</Link>
+                return <Menu.Item key={item.APP_TYPE_ID || 0}
+                  style={{height: 60, lineHeight: '60px', fontSize: 16}}>
+                  {item.APP_TYPE_NAME || '无'}
                 </Menu.Item>
               })}
             </Menu>
@@ -152,7 +263,10 @@ export default class AllApplications extends React.Component {
             <Content>
               <Route path='/operate-manage-home/all-app/all-app' render={() => {
                 // eslint-disable-next-line react/jsx-no-undef
-                return <LAllApplicationsDetail allAppListData={this.state.allAppListData} platformAppDataa={this.state.platformAppDataa} />
+                return <LAllApplicationsDetail
+                  allAppListData={this.state.allAppListData} platformAppDataa={this.state.platformAppDataa}
+                  handleDownloadNum={this.handleDownloadNum}
+                  handleShelfTime={this.handleShelfTime} />
               }} />
             </Content>
           </Layout>
