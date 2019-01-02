83d405bbf077c46ec8408b753de4fe65051f7f9e
bal-lon <Foxmail-qtum@foxmail.com>
2019-01-02 20:42:53 +0800

    修正 header 右上 消息/设置/注销 btn组高度未正确设置，未居中的问题

--- src/views/OperateManage.jsx
+++ src/views/OperateManage.jsx
@@ -286,7 +286,7 @@ class OperateManage extends React.Component {
                 : '游客'}</span>
               {webStorage.getItem('STAR_WEB_ROLE_CODE') === 'teacher' ? '老师' : ''}
             </div>
-            <div style={{height: '30px', float: 'right', marginRight: '10%'}} className='header-bar-icon'>
+            <div style={{height: '30px', float: 'right', marginRight: '10%', 'line-height': '32px'}} className='header-bar-icon'>
               <Badge count={this.state.messageCount} >
                 <Icon type='mail' style={{ fontSize: 16 }} onClick={this.handleTabChange.bind(this, '/topbar-manage/notice')} />
               </Badge>
