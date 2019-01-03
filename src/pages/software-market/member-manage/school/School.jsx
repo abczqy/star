f6a19da83a7fdabd76cff8be102d4f4bd8ca0bf7
Psd <piaoshoudong@bonc.com.cn>
2019-01-03 16:28:05 +0800

    实体管理-学校

--- src/pages/software-market/member-manage/school/School.jsx
+++ src/pages/software-market/member-manage/school/School.jsx
@@ -9,24 +9,28 @@
  * -- 还缺少--search的get数据接口
  */
 import React, { Component } from 'react'
-import { Table, Switch, Divider, message } from 'antd'
-import { Link } from 'react-router-dom'
-import ajaxUrl from 'config'
+// import { Table, Switch, Divider, message } from 'antd'
+import { Table, message } from 'antd'
+// import { Link } from 'react-router-dom'
+// import ajaxUrl from 'config'
 import {
-  schGetData,
-  maDelId,
+  // schGetData,
+  // maDelId,
   maInitPwd,
-  schBatchLeadout,
+  // schBatchLeadout,
   // getIdSelectList,
   // getNameSelectList,
   toLogin
 } from 'services/software-manage'
 import { BlankBar, SearchBarMemberSch } from 'components/software-market'
-import {
-  addKey2TableData
-  // getSelectList
-} from 'utils/utils-sw-manage'
+// import {
+//   addKey2TableData
+//   // getSelectList
+// } from 'utils/utils-sw-manage'
 import 'pages/software-market/SoftwareMarket.scss'
+import config from '../../../../config/index'
+import {axios} from '../../../../utils'
+const {API_BASE_URL_V2, SERVICE_PORTAL} = config
 
 /**
    * 表格分页器设置-默认值
@@ -47,6 +51,7 @@ class School extends Component {
         data: [],
         total: 0
       },
+      toLogin: 'all',
       reqParam: {
         shId: '',
         shName: '',
@@ -57,6 +62,7 @@ class School extends Component {
       batchLeadParams: {
         idArrs: []
       },
+      dataSource: [],
       selectList: {}
     }
   }
@@ -65,27 +71,27 @@ class School extends Component {
     return ([
       {
         title: '学校名称',
-        dataIndex: 'sh_name',
-        key: 'sh_name',
+        dataIndex: 'schoolName',
+        key: 'schoolName',
         width: 200
-      }, {
-        title: '账号',
-        dataIndex: 'sh_id',
-        key: 'sh_id',
-        width: 200
-      }, {
-        title: '所属教育机构',
-        dataIndex: 'edu_name',
-        key: 'edu_name'
-      }, {
-        title: '允许登录',
-        dataIndex: 'to_login',
-        key: 'to_login',
-        render: (text, record, index) => {
-          return (
-            <Switch checked={record.to_login === 1} onChange={() => this.handleToLogin(record)} />
-          )
-        }
+      // }, {
+      //   title: '账号',
+      //   dataIndex: 'sh_id',
+      //   key: 'sh_id',
+      //   width: 200
+      // }, {
+      //   title: '所属教育机构',
+      //   dataIndex: 'edu_name',
+      //   key: 'edu_name'
+      // }, {
+      //   title: '允许登录',
+      //   dataIndex: 'to_login',
+      //   key: 'to_login',
+      //   render: (text, record, index) => {
+      //     return (
+      //       <Switch checked={record.to_login === 1} onChange={() => this.handleToLogin(record)} />
+      //     )
+      //   }
       }, {
         title: '操作',
         dataIndex: 'options',
@@ -94,12 +100,10 @@ class School extends Component {
         render: (text, record, index) => {
           return (
             <span>
-              <Link to={{pathname: '/software-market-home/member-manage/teacher', search: record.sh_id}}>教师</Link>
+              {/* <Link to={{pathname: '/software-market-home/member-manage/teacher', search: record.sh_id}}>教师</Link>
               <Divider type='vertical' />
               <Link to={{pathname: '/software-market-home/member-manage/student', search: record.sh_id}}>学生</Link>
-              <Divider type='vertical' />
-              <a href='javascript:void(0)' onClick={(e) => this.initPwd(record)}>重置密码</a>
-              <Divider type='vertical' />
+              <Divider type='vertical' /> */}
               <a href='javascript:void(0)' onClick={(e) => this.delId(record)}>删除</a>
             </span>
           )
@@ -107,24 +111,24 @@ class School extends Component {
       }])
   }
 
-  getParams = () => {
-    let a = window.location.href.split('?')
-    const {
-      shId,
-      shName,
-      eduServices,
-      loginType
-    } = this.state.reqParam
-    return {
-      pageSize: this.state.pagination.pageSize,
-      pageNum: this.state.pagination.pageNum,
-      sh_id: shId || '',
-      shName: shName || '',
-      eduServices: eduServices || '',
-      loginType: loginType || '',
-      id: a[1] || ''
-    }
-  }
+  // getParams = () => {
+  //   let a = window.location.href.split('?')
+  //   const {
+  //     shId,
+  //     shName,
+  //     eduServices,
+  //     loginType
+  //   } = this.state.reqParam
+  //   return {
+  //     pageSize: this.state.pagination.pageSize,
+  //     pageNum: this.state.pagination.pageNum,
+  //     sh_id: shId || '',
+  //     shName: shName || '',
+  //     eduServices: eduServices || '',
+  //     loginType: loginType || '',
+  //     id: a[1] || ''
+  //   }
+  // }
 
   /**
    * 获取运营中的应用列表数据
@@ -132,14 +136,42 @@ class School extends Component {
    * 用一个程序-专门转换后台数据-给每一条记录加上key值--把自身的fa_id映射过去即可
    */
   getTableDatas = () => {
-    schGetData(this.getParams(), (res) => {
-      const data = res.data
-      this.setState({
-        tableData: {
-          data: addKey2TableData(data.list, 'sh_id'),
-          total: data.total
-        }
-      })
+    let param = {
+      pageNum: this.state.pagination.pageNum,
+      pageSize: this.state.pagination.pageSize
+    }
+    // if (this.state.USER_ACCOUNT !== '') {
+    //   // 后台要求，回传数据的时候字段为小写
+    //   param.account = this.state.USER_ACCOUNT
+    // }
+    // if (this.state.USER_NAME !== '') {
+    //   // 后台要求，回传数据的时候字段为小写
+    //   param.name = this.state.USER_NAME
+    // }
+    // if (this.state.AUTHORITY_NAME !== '') {
+    //   // 后台要求，回传数据的时候字段为小写
+    //   param.organization_name = this.state.AUTHORITY_NAME
+    // }
+    if (this.state.toLogin !== 'all') {
+      if (this.state.toLogin === 'allow') {
+        param.login = 1
+      } else {
+        param.login = 0
+      }
+    }
+    axios.get(`${API_BASE_URL_V2}${SERVICE_PORTAL}/schools`, {params: param}).then((res) => {
+      if (res.data.code === 200) {
+        console.log(res.data.data)
+        // this.setState({
+        //   dataSource: res.data.data.content,
+        //   total: res.data.data.totalElements
+        // })
+        this.setState({
+          dataSource: res.data.data
+        })
+      } else {
+        message.warn(res.data.msg)
+      }
     })
   }
 
@@ -208,21 +240,8 @@ class School extends Component {
    * 当下拉选择框‘允许登录’值改变时回调
    */
   onToLogin = (val) => {
-    console.log(`val: ${val}`)
-    let loginAllow = ''
-    if (val === 'allow') {
-      loginAllow = '1'
-    } else if (val === 'refuse') {
-      loginAllow = '0'
-    } else if (val === 'all') {
-      loginAllow = ''
-    }
-    // 修改state.reqParams中对应的值
     this.setState({
-      reqParam: {
-        ...this.state.reqParam,
-        loginType: loginAllow
-      }
+      toLogin: val
     })
   }
 
@@ -231,12 +250,12 @@ class School extends Component {
    */
   delId = (record) => {
     // 还缺一个参数 后台未提供 "user_type": 1//用户类型
-    const params = {
-      sh_id: record.sh_id
-    }
-    maDelId(params, (res) => {
-      console.log(`res.data.result: ${res.data.result}`)
-    })
+    // const params = {
+    //   sh_id: record.sh_id
+    // }
+    // maDelId(params, (res) => {
+    //   console.log(`res.data.result: ${res.data.result}`)
+    // })
     // 最好有个确认的弹窗什么的
     // 后面再加上loading + 操作成功的提示
   }
@@ -272,21 +291,21 @@ class School extends Component {
    */
   search = () => {
     console.log(`开始搜索: ${JSON.stringify(this.getParams())}`)
-    this.getTableDatas()
+    // this.getTableDatas()
   }
 
   /**
    * 当点击'批量导出'按钮时的回调
    */
-  onBatchLeadout = () => {
-    // 从state中获取实时的stu_id数组的值 作为请求参数传给后台
-    const { idArrs } = this.state.batchLeadParams
-    console.log(`IdArrs: ${JSON.stringify(idArrs)}`)
-    schBatchLeadout({sh_id: idArrs}, (res) => {
-      window.open(ajaxUrl.IMG_BASE_URL + '/' + res.data.info)
-      console.log(`${res.data.info}`)
-    })
-  }
+  // onBatchLeadout = () => {
+  //   // 从state中获取实时的stu_id数组的值 作为请求参数传给后台
+  //   const { idArrs } = this.state.batchLeadParams
+  //   console.log(`IdArrs: ${JSON.stringify(idArrs)}`)
+  //   schBatchLeadout({sh_id: idArrs}, (res) => {
+  //     window.open(ajaxUrl.IMG_BASE_URL + '/' + res.data.info)
+  //     console.log(`${res.data.info}`)
+  //   })
+  // }
 
   /**
    * 多选选项变化
@@ -347,7 +366,7 @@ class School extends Component {
   }
 
   render () {
-    const { pagination, tableData, selectList } = this.state
+    const { pagination, selectList } = this.state
     return (
       <div className='software-wrap'>
         <SearchBarMemberSch
@@ -362,16 +381,16 @@ class School extends Component {
         <BlankBar />
         <Table
           columns={this.getColumns()}
-          dataSource={tableData.data}
+          dataSource={this.state.dataSource}
           pagination={{
             ...pagination,
-            total: this.state.tableData.total,
+            total: this.state.dataSource.length,
             onShowSizeChange: this.onShowSizeChange,
             onChange: this.pageNumChange
           }}
-          rowSelection={{
-            onChange: this.rowSelectChange
-          }}
+          // rowSelection={{
+          //   onChange: this.rowSelectChange
+          // }}
         />
       </div>
     )
