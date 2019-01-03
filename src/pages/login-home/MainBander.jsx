ee319937d3c00db7cf24534ad5c2658ccd6c3adf
qiaoyang <qiaoyang@bonc.com.cn>
2019-01-03 10:34:04 +0800

    bug-门户首页的banner被纵向拉伸了

--- src/pages/login-home/MainBander.jsx
+++ src/pages/login-home/MainBander.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types,react/jsx-no-bind */
 import React from 'react'
 import Slider from 'react-slick'
-import { Row, Form, Icon, Input, Button } from 'antd'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './MainHome.scss'
@@ -24,23 +23,11 @@ class MainBander extends React.Component {
     this.state = {
       userName: '', // 用户名密码
       passWord: '', // 密码
-      loginFormVisible: this.getDefaultLoginFormVisible(),
       msgTip: '', // 用户登陆信息提示
       bannerImg: [],
       levelList: [], // 年级下拉列表值
       dutyList: []// 行政职务下拉列表值
     }
-  }
-
-  getDefaultLoginFormVisible () {
-    // debugger
-    let flag = false
-    // let name = getCookie('id')
-    if (webStorage.getItem('STAR_WEB_ROLE_CODE') === '' || webStorage.getItem('STAR_WEB_ROLE_CODE') === null) {
-      flag = true
-      webStorage.clear()
-    }
-    return flag
   }
 
   handleLogin () {
@@ -75,24 +62,11 @@ class MainBander extends React.Component {
         if (data.isFirstLogged) {
           // 如果是教师和学生   先弹出信息
           if (data.roleCode === 'teacher' || data.roleCode === 'students') {
-            this.setState({
-              loginFormVisible: false
-            })
             this.props.showSureWin(this.getSureInfoData(data.personInfo || {}, data.roleCode))
-          } else if (data.roleCode === 'parents') { // 如果是家长  关闭登录form框
-            this.setState({
-              loginFormVisible: false
-            })
           } else { // 其他的弹出修改密码窗口
-            this.setState({
-              loginFormVisible: false
-            })
             this.props.handleChangeVisible('changeInfoWinVisible', true)
           }
         } else {
-          this.setState({
-            loginFormVisible: false
-          })
           if (data.roleCode === 'operator') {
             this.props.history.push({
               pathname: '/software-market-home'
@@ -229,52 +203,16 @@ class MainBander extends React.Component {
           <Slider {...settings}>
             {
               this.state.bannerImg.map((item, index, arr) => {
-                return item.picUrl ? <div key={index} style={{height: '445px', width: '100%'}} >
-                  <img src={Config.IMG_BASE_URL_V2 + item.picUrl || ''} style={{height: '445px', width: '100%'}} />
+                return item.picUrl ? <div key={index} style={{height: '228px', width: '100%'}} >
+                  <img src={Config.IMG_BASE_URL_V2 + item.picUrl || ''} style={{height: '100%', width: '100%'}} />
                 </div>
-                  : <div key={index} style={{height: '445px', width: '100%'}} >
-                    <img src={imgBanner} style={{height: '445px', width: '100%'}} />
+                  : <div key={index} style={{height: '228px', width: '100%'}} >
+                    <img src={imgBanner} style={{height: '100%', width: '100%'}} />
                   </div>
               })
             }
           </Slider>
         </div>
-        {
-          this.state.loginFormVisible ? (
-            <div className='login-div-container' >
-              <Row>
-                <p style={{marginTop: '20px', textAlign: 'center', fontFamily: "'PingFangSC-Semibold', 'PingFang SC Semibold', 'PingFang SC'", fontSize: '20px', color: '#FFFFFF', fontStyle: 'normal'}}>用户登录</p>
-              </Row>
-              <Row>
-                <Form style={{marginLeft: '15px', marginRight: '15px'}}>
-                  <Form.Item>
-                    <Input onChange={(e) => { this.handleValueChange(e, 'userName') }} value={this.state.userName} className='custom-input' prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder='请输入用户名' />
-                  </Form.Item>
-                  <Form.Item>
-                    <Input onChange={(e) => { this.handleValueChange(e, 'passWord') }} value={this.state.passWord} className='custom-input' prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder='请输入密码' type='password' />
-                  </Form.Item>
-                  <Form.Item>
-                    <a style={{color: 'red', fontSize: '12px', float: 'left'}} href=''>{this.state.msgTip}</a>
-                    <a style={{color: 'white', fontSize: '12px', float: 'right'}} onClick={this.handregister.bind(this, '/forget-home')}>忘记密码?</a>
-                  </Form.Item>
-                  <Form.Item>
-                    <Button type='primary' htmlType='submit' className='login-btn'
-                      onClick={this.handleLogin.bind(this)}>
-                      立即登录
-                    </Button>
-                  </Form.Item>
-                  <Form.Item>
-                    <Button type='primary' htmlType='submit' className='register-btn' onClick={this.handregister.bind(this, '/register-home')}>
-                      注册账号
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </Row>
-            </div>
-          ) : null
-        }
       </div>
     )
   }
