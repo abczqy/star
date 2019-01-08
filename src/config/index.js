313e2f512266936fb0bbd95f7c586b0e523f81e2
bal-lon <Foxmail-qtum@foxmail.com>
2019-01-08 19:32:59 +0800

    增加 cd 代理配置

--- src/config/index.js
+++ src/config/index.js
@@ -13,15 +13,18 @@ const Config = {
   API_BASE_URL_AUTHENTICATION: 'http://192.168.1.71:10200', // 登陆接口
   // API_BASE_URL_V2: 'http://192.168.1.31:10101', // 服务器总路径
   API_BASE_URL_V2: 'http://f192x88923.iok.la:10101', // 服务器总路径-北京访问
+  // API_BASE_URL_V2: 'http://10.100.14.7:10101', // 服务器总路径-福建测试服务器
   SERVICE_PORTAL: '/portal', // 门户
   SERVICE_EDU_MARKET: '/edu-market', // 市场
   SERVICE_AUTHENTICATION: '/authentication', // 用户认证
   // IMG_BASE_URL_V2: 'http://192.168.1.31:4400/', // 文件服务器-图片
   // DOC_BASE_URL_V2: 'http://192.168.1.31:4400/', // 文件服务器-文档
-  // SOFT_BASE_URL_V2: 'http://192.168.1.31:4400/' // 文件服务器-软件
+  // SOFT_BASE_URL_V2: 'http://192.168.1.31:4400/', // 文件服务器-软件
   IMG_BASE_URL_V2: 'http://f192x88923.iok.la:4400/', // 文件服务器-图片-bj
   DOC_BASE_URL_V2: 'http://f192x88923.iok.la:4400/', // 文件服务器-文档-bj
   SOFT_BASE_URL_V2: 'http://f192x88923.iok.la:4400/' // 文件服务器-软件-bj
+  // IMG_BASE_URL_V2: 'http://10.100.14.7:443/', // 文件服务器-图片-福建测试服务器
+  // DOC_BASE_URL_V2: 'http://10.100.14.7:443/', // 文件服务器-福建测试服务器
+  // SOFT_BASE_URL_V2: 'http://10.100.14.7:443/' // 文件服务器-福建测试服务器
 }
-
 export default Config
